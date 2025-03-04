2a15a52eebcc91ce67fd5660af963a382ea18b4e
dpi7ot <dpi7ot@gmail.com>
2025-03-04 10:24:09 +0530

    Add files via upload

---
+++ lbp18.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	float a,b,c,S,A;
+	printf("enter  the values of a,b,c,");
+	scanf("%f%f%f",&a,&b,&c);
+	S=(a+b+c)/2;
+	A=sqrt(s*(s-a)*(s-b)*(s-c));
+	printf("area of triangle is %f",A);
+	getch();
+}
