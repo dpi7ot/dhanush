b61c16981f1e8ca59d49451bf50a54cdb37f8881
dpi7ot <dpi7ot@gmail.com>
2025-03-04 09:50:18 +0530

    Add files via upload

---
+++ lbp10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,s;
+	printf("enter the number of numbers");
+	scanf("%d",&n);
+	s=(n*(n+1))/2;
+	printf("sumof first natural numbers is %d,",s);
+getch();	
+}
